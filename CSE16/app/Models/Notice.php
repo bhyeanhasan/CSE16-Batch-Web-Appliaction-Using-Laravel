88513c7554e2429487e3701f1f67e311c7e5c85d
Md. Babul Hasan <bhyean@gmail.com>
2020-11-07 23:59:32 +0600

    Notice Model
    added notice

---
+++ CSE16/app/Models/Notice.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Notice extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'notice_heading',
+        'notice_body',
+        'notice_link',
+        'notice_picture',
+    ];
+}
---
+++ CSE16/database/migrations/2020_11_07_174808_create_notices_table.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateNoticesTable extends Migration
+{
+
+    public function up()
+    {
+        Schema::create('notices', function (Blueprint $table) {
+            $table->id();
+            $table->string('notice_heading');
+            $table->string('notice_body');
+            $table->string('notice_link');
+            $table->string('notice_picture');
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('notices');
+    }
+}
